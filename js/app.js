cedf10cc8a22abba96b8c090ff79ac75dbaafe3b
Talha-Bayansar <talha.bayansar@hotmail.com>
2021-03-02 16:00:04 +0100

    added notification

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+let swRegistration = null;
+
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
             .register("/serviceWorker.js")
-            .then((res) => console.log("service worker registered"))
+            .then((res) => {
+                console.log("service worker registered");
+                swRegistration = res;
+            })
             .catch((err) => console.log("service worker not registered", err));
     });
 }
@@ -55,11 +60,45 @@ const showCoffees = () => {
                 <div class="card">
                   <img class="card--avatar" src=${image} />
                   <h1 class="card--title">${name}</h1>
-                  <a class="card--link" href="#">Taste</a>
+                  <button class="card--link">Taste</button>
                 </div>
                 `)
     );
     container.innerHTML = output;
+    document
+        .querySelectorAll(".card--link")
+        .forEach((b) => b.addEventListener("click", displayNotification));
 };
 
 document.addEventListener("DOMContentLoaded", showCoffees);
+
+function displayNotification() {
+    if (window.Notification && Notification.permission === "granted") {
+        notification();
+    }
+    // If the user hasn't told if he wants to be notified or not
+    // Note: because of Chrome, we are not sure the permission property
+    // is set, therefore it's unsafe to check for the "default" value.
+    else if (window.Notification && Notification.permission !== "denied") {
+        Notification.requestPermission((status) => {
+            if (status === "granted") {
+                notification();
+            } else {
+                alert("You denied or dismissed permissions to notifications.");
+            }
+        });
+    } else {
+        // If the user refuses to get notified
+        alert(
+            "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
+        );
+    }
+}
+
+function notification() {
+    console.log("clicked");
+    const options = {
+        body: "Testing Our Notification",
+    };
+    swRegistration.showNotification("PWA Notification!", options);
+}
